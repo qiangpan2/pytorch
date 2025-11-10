91377fce4972b88efa17c68921e939a9b38eee82
qiangpan <qiangpan@amd.com>
2025-11-10 08:40:22 +0000

    channellast3

--- aten/src/ATen/native/ConvUtils.h
+++ aten/src/ATen/native/ConvUtils.h
@@ -375,7 +375,9 @@ inline bool miopen_conv_use_channels_last(const at::Tensor& input, const at::Ten
             (weight_memory_format == at::MemoryFormat::ChannelsLast) )
         );
 
-  bool can_use_miopen_channels_last_3d = false;
+  bool can_use_miopen_channels_last_3d =
+      (input_memory_format  == at::MemoryFormat::ChannelsLast3d) ||
+      (weight_memory_format == at::MemoryFormat::ChannelsLast3d);
 
   return can_use_miopen_channels_last_2d || can_use_miopen_channels_last_3d;
 }
