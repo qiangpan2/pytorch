009abd6e5f8489657512fb6413051fbdba82caf3
qiang <qiangpan@amd.com>
2025-12-22 20:27:08 +0800

    revert force channel last

--- aten/src/ATen/native/ConvUtils.h
+++ aten/src/ATen/native/ConvUtils.h
@@ -389,14 +389,6 @@ inline at::MemoryFormat miopen_conv_suggest_memory_format(const at::Tensor& inpu
     return at::MemoryFormat::ChannelsLast3d;
   }
 
-  if (suggest_nhwc && (weight_ndim == 4)) {
-    return at::MemoryFormat::ChannelsLast;
-  }
-
-  if (suggest_nhwc && (weight_ndim == 5)) {
-    return at::MemoryFormat::ChannelsLast3d;
-  }
-
   return at::MemoryFormat::Contiguous;
 }
 
